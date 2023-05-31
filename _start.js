f99750bc32e81729deeb997c9bb8d1f55907c17e
PapaCreator <135080623+PapaCreatorBot@users.noreply.github.com>
2023-06-01 06:08:01 +0800

    Create _start.js

---
+++ _start.js
@@ -0,0 +1,24 @@
+var admin = Bot.getProperty("#admin")
+  if (admin == user.telegramid) {
+    var url = Libs.Webhooks.getUrlFor({
+      command: lib.commandOnWebhook(),
+      user_id: user.id
+    })
+    var create = WebApp.getUrl({
+      command: "#creates",
+      options: { url: url }
+    })
+
+    Api.sendMessage({
+      text:
+        "👋 Hi, " +
+        user.first_name +
+        " I can help you to manage your bot using my command\n\n/mycommand - command list\n/edit - [command name]\n/preview - Click to preview your work.",
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "📝Create a new command", web_app: { url: create } }]
+        ]
+      }
+    })
+    return
+  }
