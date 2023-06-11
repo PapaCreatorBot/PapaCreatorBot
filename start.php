748d0c39cfe60258c777d71bbacad0b820365072
PapaCreator <135080623+PapaCreatorBot@users.noreply.github.com>
2023-06-11 11:31:43 +0800

    Update start.php

--- start.php
+++ start.php
@@ -1,4 +1,7 @@
 <?php
-//php
-$Link = ""
+require 'botdaftar/vendor/autoload.php';
+$bot = new PHPTelebot('5729026585:AAFqY_uwB8EQnl7UYZnCKoO6iyA_FU5L9Fc','PapaCreatorBot');
+$bot->cmd('/start', 'the test is starting..');
+$bot->cmd('/test', 'hi';
+$bot->run();
 php?>
