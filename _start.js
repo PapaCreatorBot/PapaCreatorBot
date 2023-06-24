51871d8ac5cf7001b61595b0b29f2691894f0b91
PapaCreator <135080623+PapaCreatorBot@users.noreply.github.com>
2023-06-25 05:38:18 +0800

    Update _start.js

--- _start.js
+++ _start.js
@@ -11,10 +11,13 @@ var admin = Bot.getProperty("#admin")
     }) 
 
 Api.sendMessage({
-text : `👋Hi, ` +
-        user.first_name +
-        ` Manage your bot using command.\n\n/mycommand - command list.\n/edit - editing command.\n/preview - click to preview your work.\n\n💎 <a href="https://github.com/PapaCreatorBot/Help/blob/main/Plan.md#what-it----iteration">Iteration</a>: ` +
-        Libs.iterations().value(),
+text : `👋Hi, ` ±
+        user.first_name ±
+        ` Manage your bot using command.\n\n/mycommand - command list.\n/edit - editing command.\n/preview - click to preview your work.\n\n💎 <a href="https://github.com/PapaCreatorBot/Help/blob/main/Plan.md#what-it----iteration">Iteration</a>: <b>` ±
+        Libs.Iterations.value() ±
+        `</b>\n♻️ Next Payment in: <b>` ±
+        resets() ±
+        ` Days</b>`,
       disable_web_page_preview: true,
       parse_mode: "html",
       reply_markup: {
